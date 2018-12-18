f9e4eacf82d4b889595551fb37a028a1c8a9c660
Nguyen Duong <nnbduong@gmail.com>
2018-12-18 20:26:57 +0700

    điều chỉnh router khi đăng nhập
    điều chỉnh router khi đăng nhập

--- _frontend/src/router.js
+++ _frontend/src/router.js
@@ -9,8 +9,7 @@ import Login from './views/auth/Login.vue';
 
 Vue.use(Router);
 
-export default new Router({
-	mode: 'history',
+var router = new Router({
 	routes: [
 		{
 			path: '/',
@@ -20,22 +19,26 @@ export default new Router({
 		{
 			path: '/App1',
 			name: 'App1',
-			component: App1
+			component: App1,
+			meta: { requiresAuth: true, App1Auth: true }
 		},
 		{
 			path: '/App2',
 			name: 'App2',
-			component: App2
+			component: App2,
+			meta: { requiresAuth: true, App2Auth: true }
 		},
 		{
 			path: '/App3',
 			name: 'App3',
-			component: App3
+			component: App3,
+			meta: { requiresAuth: true, App3Auth: true }
 		},
 		{
 			path: '/App4',
 			name: 'App4',
-			component: App4
+			component: App4,
+			meta: { requiresAuth: true, App4Auth: true }
 		},
 		{
 			path: '/Login',
@@ -48,3 +51,79 @@ export default new Router({
 		}
 	]
 });
+
+router.beforeEach((to, from, next) => {
+	if (to.meta.requiresAuth) {
+		var authUser = localStorage.role;
+		var token = localStorage.access_token;
+		//console.log(authUser + `|` + token);
+		if (!authUser || !token) {
+			console.log('route to login');
+			next({ name: 'Login' });
+		} else {
+			if (to.matched.some(record => record.meta.App1Auth)) {
+				if (authUser == '1') {
+					next();
+				} else if (authUser == '2') {
+					next({ name: 'App2' });
+				} else if (authUser == '3') {
+					next({ name: 'App3' });
+				} else if (authUser == '4') {
+					next({ name: 'App4' });
+				}
+			} else if (to.matched.some(record => record.meta.App2Auth)) {
+				if (authUser == '2') {
+					next();
+				} else if (authUser == '1') {
+					next({ name: 'App1' });
+				} else if (authUser == '3') {
+					next({ name: 'App3' });
+				} else if (authUser == '4') {
+					next({ name: 'App4' });
+				}
+			} else if (to.matched.some(record => record.meta.App3Auth)) {
+				if (authUser == '3') {
+					next();
+				} else if (authUser == '1') {
+					next({ name: 'App1' });
+				} else if (authUser == '2') {
+					next({ name: 'App2' });
+				} else if (authUser == '4') {
+					next({ name: 'App4' });
+				}
+			} else if (to.matched.some(record => record.meta.App4Auth)) {
+				if (authUser == '4') {
+					next();
+				} else if (authUser == '1') {
+					next({ name: 'App1' });
+				} else if (authUser == '2') {
+					next({ name: 'App2' });
+				} else if (authUser == '3') {
+					next({ name: 'App3' });
+				}
+			} else {
+				next({ name: 'Login' });
+			}
+		}
+		//  else if (to.meta.App1Auth) {
+		// 	if (authUser == '1') {
+		// 		next();
+		// 	} else {
+		// 		next('/Login');
+		// 		console.log('route to login from app 1');
+		// 	}
+		// } else if (to.meta.App2Auth) {
+		// 	//const authUser = JSON.parse(window.localStorage.getItem('lbUser'))
+		// 	if (authUser == '2') {
+		// 		next();
+		// 	} else {
+		// 		next('/Login');
+		// 		console.log('route to login from app 2');
+		// 	}
+		// }
+	} else {
+		next();
+	}
+});
+
+export default router;
